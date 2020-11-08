4ee34c4599a91220095426bdba008dc5941c0307
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-11-08 23:04:36 +0530

    Check Float Maximum Value

--- src/main/java/FindMax.java
+++ src/main/java/FindMax.java
@@ -1,6 +1,7 @@
 package com.generictest;
 
 public class FindMax {
+	//Function for Integer maximum Value
 	public Integer checkMax(Integer firstValue, Integer secondValue, Integer thirdValue) {
 		Integer max = firstValue;
 		if (secondValue.compareTo(max) > 0) {
@@ -11,3 +12,16 @@ public class FindMax {
 		}
 		return max;
 	}
+
+	//Function for check Maximum float value
+	public Float checkMax(Float firstValue, Float secondValue, Float thirdValue) {
+		Float max = firstValue;
+		if (secondValue.compareTo(max) > 0) {
+			max = secondValue;
+		}
+		if (thirdValue.compareTo(max) > 0) {
+			max = thirdValue;
+		}
+		return max;
+	}
+}
--- src/test/java/GenericTestClass.java
+++ src/test/java/GenericTestClass.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class GenericTestClass {
 
-	//Test case for greatest FirstValue True
+	//Test case for greatest Integer FirstValue True
 	@Test
 	public void FirstMaximumIntegerValue_FirstPositionTrue() {
 		Integer firstValue = 40, secondValue = 30, thirdValue = 25;
@@ -15,7 +15,7 @@ public class GenericTestClass {
 		Assert.assertEquals(firstValue, max);
 	}
 
-	 //Test case for greatest FirstValue False
+	 //Test case for greatest Integer FirstValue False
         @Test
         public void FirstMaximumIntegerValue_FirstPositionFalse() {
                 Integer firstValue = 20, secondValue = 30, thirdValue = 25;
@@ -24,7 +24,7 @@ public class GenericTestClass {
                 Assert.assertNotEquals(firstValue, max);
         }
 
-	//Test case for greatest SecondValue True
+	//Test case for greatest Integer SecondValue True
 	@Test
 	public void secondMaximumIntegerValue_SecondPositionTrue() {
 		FindMax main = new FindMax();
@@ -33,7 +33,7 @@ public class GenericTestClass {
 		Assert.assertEquals(secondValue, max);
 	}
 
-	 //Test case for greatest SecondValue False
+	 //Test case for greatest Integer SecondValue False
         @Test
         public void secondMaximumIntegerValue_SecondPositionTrue() {
                 FindMax main = new FindMax();
@@ -42,7 +42,7 @@ public class GenericTestClass {
                 Assert.assertNotEquals(secondValue, max);
         }
 
-	//Test case for greatest ThirdValue True
+	//Test case for greatest Integer ThirdValue True
 	@Test
 	public void thirdMaximumIntegerValue_ThirdsPositionTrue() {
 		FindMax main = new FindMax();
@@ -51,7 +51,7 @@ public class GenericTestClass {
 		Assert.assertEquals(thirdValue, max);
 	}
 
-	//Test case for greatest ThirdValue False
+	//Test case for greatest Integer ThirdValue False
         @Test
         public void thirdMaximumIntegerValue_ThirdPositionFalse() {
                 FindMax main = new FindMax();
@@ -60,4 +60,60 @@ public class GenericTestClass {
                 Assert.assertNotEquals(thirdValue, max);
         }
 
+	// Test case for greatest Float FirstValue True
+	@Test
+	public void FirstMaximumFloatValue_FirstPositionTrue() {
+		FindMax main = new FindMax();
+		Float firstValue = 5.6f, secondValue = 2.5f, thirdValue = 3.5f;
+		Float max = main.checkFloatmax(firstValue, secondValue, thirdValue);
+		Assert.assertEquals(firstValue, max);
+	}
+
+	
+        // Test case for greatest Float FirstValue False
+        @Test
+        public void FirstMaximumFloatValue_FirstPositionFalse() {
+                FindMax main = new FindMax();
+                Float firstValue = 1.6f, secondValue = 2.5f, thirdValue = 3.5f;
+                Float max = main.checkFloatmax(firstValue, secondValue, thirdVa>
+                Assert.assertNotEquals(firstValue, max);
+        }
+
+
+	// Test case for greatest Float Second True
+	@Test
+	public void secondMaximumFloatValue_SecondtPositionTrue() {
+		FindMax main = new FindMax();
+		Float firstValue = 5.6f, secondValue = 6.5f, thirdValue = 3.5f;
+		Float max = main.checkFloatmax(firstValue, secondValue, thirdValue);
+		Assert.assertEquals(secondValue, max);
+	}
+
+	 // Test case for greatest Float Second False
+        @Test
+        public void secondMaximumFloatValue_SecondtPositionFalse() {
+                FindMax main = new FindMax();
+                Float firstValue = 5.6f, secondValue = 2.5f, thirdValue = 3.5f;
+                Float max = main.checkFloatmax(firstValue, secondValue, thirdVa>
+                Assert.assertNotEquals(secondValue, max);
+        }
+
+
+	// Test case for greatest Float ThirdValue True
+	@Test
+	public void thirdMaximumFloatValue_ThirdPositionTrue() {
+		FindMax main = new FindMax();
+		Float firstValue = 5.6f, secondValue = 6.5f, thirdValue = 7.5f;
+		Float max = main.checkFloatmax(firstValue, secondValue, thirdValue);
+		Assert.assertEquals(thirdValue, max);
+	}
+
+        // Test case for greatest Float ThirdValue False
+        @Test
+        public void thirdMaximumFloatValue_ThirdPositionFalse() {
+                FindMax main = new FindMax();
+                Float firstValue = 5.6f, secondValue = 6.5f, thirdValue = 3.5f;
+                Float max = main.checkFloatmax(firstValue, secondValue, thirdVa>
+                Assert.assertNotEquals(thirdValue, max);
+        }
 }
