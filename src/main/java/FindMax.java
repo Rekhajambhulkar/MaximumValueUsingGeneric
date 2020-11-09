51abc29b916085fb6aa8b4232b6db8fe56632dc3
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-11-09 12:57:31 +0530

    Sort the maximum value

--- src/main/java/FindMax.java
+++ src/main/java/FindMax.java
@@ -2,28 +2,27 @@ package com.generictest;
 
 public class FindMax<T extends Comparable<T>> {
 	FindMax main;
-	T firstValue, secondValue, thirdValue;
+	T[] value;
 
-	public FindMax(T firstValue, T secondValue, T thirdValue) {
-		this.firstValue = firstValue;
-		this.secondValue = secondValue;
-		this.thirdValue = thirdValue;
+	public public FindMax(T... value) {
+		this.value = value;
 	}
 
-	// Function for check All type of Maximum values
-	public static <T extends Comparable> T checkMax(T firstValue, T secondValue, T thirdValue) {
-		T max = firstValue;
-		if (secondValue.compareTo(max) > 0) {
-			max = secondValue;
-		}
-		if (thirdValue.compareTo(max) > 0) {
-			max = thirdValue;
-		}
+	// Function for check Maximum value and sort it
+	public static <T extends Comparable> T checkMax(T[] value) {
+		Arrays.sort(value);
+		int size = value.length;
+		T max = value[size - 1];
+		checkMaxValue(value, max);
+		return max;
+	}
+
+	private static <T extends Comparable> T checkMaxValue(T[] value, T max) {		
 		return max;
 	}
 
 	// Function for check internally max value
-	public Comparable checkMaxValue() {
-		return main.checkMax(firstValue, secondValue, thirdValue);
+	public Comparable checkMax() {
+		return main.checkMax(value, max);
 	}
 }
